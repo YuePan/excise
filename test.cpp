7dc35a93dd166d2e8e8e2662b345afbce0c8edeb
Yue Pan <panyuemail@gmail.com>
2013-10-09 19:47:57 +0800

    Second committed files

--- test.cpp
+++ test.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 int main()
 {
-    std::cout << "Please enter two numbers: " << std::endl;
+    std::cout << "Please just enter two numbers: " << std::endl;
     int v1 = 0, v2 = 0;
     std::cin >> v1 >> v2;
     std::cout << "The sum of " << v1 << " and " << v2
---
+++ xcodeexec.h
@@ -0,0 +1,14 @@
+//
+//  xcodeexec.h
+//  
+//
+//  Created by PanYue on 9/18/13.
+//
+//
+
+#ifndef _xcodeexec_h
+#define _xcodeexec_h
+
+
+
+#endif
